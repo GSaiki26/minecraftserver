90a9081898b9c76ac3b1bd9a82c387c37b82245e
Saiki <GSaiki26@gmail.com>
2023-10-11 16:46:19 -0300

    Added: Vanilla/Custom working

---
+++ README.md
@@ -0,0 +1,23 @@
+# Minecraft Server Image
+The minecraft server image is a docker image that has the only objetive to start a Vanilla/Forge server.
+
+In order to create your own server, you `need` to create an environment file. In the root folder from the [Repository](https://github.com/GSaiki26/minecraftserver-image) has a file named `server.env`, an env example.
+
+## Environment Variables
+The current environment varibles are:
+* `XMS` - It'll define the minimum RAM to run the server;
+* `XMX` - It'll define the maximum RAM to run the server;
+* `SERVER_URL` - The direct URL from the server to download;
+* `FORGE_SERVER_URL` - The direct URL from the Forge server to download;
+* `FORGE_MINECRAFT_VERSION` - The minecraft version to be used to download the forge;
+* `FORGE_VERSION` - The forge version to be downloaded.
+
+The fields dont need necessarilly be defined. Check below:
+* ( Vanilla ) If you want a vanilla server, just insert the direct url into the `SERVER_URL` field;
+* ( Forge ) define the `FORGE_MINECRAFT_VERSION` and the `FORGE_VERSION` to download. Otherwise, you can simply insert the direct url from the installer in the `FORGE_SERVER_URL`.
+* ( Custom ) If you want your simple .jar file, you can insert the direct link on `VANILLA_SERVER_URL` environment variable, or just replace the ./server.jar file that's on `/server` folder. The main command can be found in the script: `./run.sh`.
+
+``Obs``: Before, the minecraft had a simple link that could replace the game's version, but currently it uses a hash, making impossible ( without mapping ) to dynamically download the desired version.
+
+## Volumes
+The server folder containing all the contents from your server can be found on `/server`.
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,9 @@ function start {
 }
 
 # Code
-
 # Check if the server folder is reacheable
 cd /server
-if [ ! -d "/server" ] || [ ! -w "/server" ]; then
+if [[ (! -d "/server" || ! -w "/server") ]]; then
   echo "The /server folder is not reacheable. Please check it permissions."
   exit 1
 fi
@@ -19,31 +18,38 @@ fi
 printf "Starting server with $(java --version)\n\n";
 
 # Check if the server already is ready.
-if [ -f "./eula.txt" ] && [ -r "./eula.txt" ]; then
+if [[ (-f "./eula.txt" && -r "./eula.txt") ]]; then
   # Start the server.
   echo "Server already exists, running it...";
   start;
 fi
 
-# The server is new. Check if a forge version was provided.
-if [ "$MINECRAFT_VERSION" != "" ] && [ "$FORGE_VERSION" != "" ]; then
-  # Download the server.
-  VERSION="$MINECRAFT_VERSION-$FORGE_VERSION"
-  echo "Installing forge $VERSION...";
-  echo "Downloading from: https://maven.minecraftforge.net/net/minecraftforge/forge/$VERSION/forge-$VERSION-installer.jar";
-  wget -q "https://maven.minecraftforge.net/net/minecraftforge/forge/$VERSION/forge-$VERSION-installer.jar";
+# The server is new. Check if the forge envs were provided.
+if [[ ("$FORGE_MINECRAFT_VERSION" != "" && "$FORGE_VERSION" != "") || "$FORGE_SERVER_URL" != "" ]]; then
+  SERVER_TO_DOWNLOAD=$FORGE_SERVER_URL
+  if [[ ("$FORGE_MINECRAFT_VERSION" != "" && "$FORGE_VERSION" != "") ]]; then
+    VERSION="$FORGE_MINECRAFT_VERSION-$FORGE_VERSION"
+    SERVER_TO_DOWNLOAD="https://maven.minecraftforge.net/net/minecraftforge/forge/$VERSION/forge-$VERSION-installer.jar"
+  fi
+
+  echo "Installing forge server from: $SERVER_TO_DOWNLOAD";
+  wget -q $SERVER_TO_DOWNLOAD -O ./server.jar;
 
   # Install the server.
-  java -jar "./forge-$VERSION-installer.jar" --installServer /server;
-  rm forge-*;
+  java -jar ./server.jar --installServer /server;
+  rm server.jar* run.bat;
   start;
   return
 fi
 
 # The forge was not defined. So the vanilla was choosen.
-if [ -r "./server.jar" ]; then
-  echo "Creating the files to automatically run the server on next boot...";
+if [ "$SERVER_URL" != "" ]; then
+  echo "Installing vanilla server from: $SERVER_URL";
+  wget -q $SERVER_URL -O ./server.jar;
   printf "#! /bin/bash\njava -jar @user_jvm_args.txt ./server.jar" > ./run.sh;
+  chmod u+x ./run.sh;
   start;
   return
 fi
+
+echo "Any version from minecraft was specified. Please check your environment variables."
